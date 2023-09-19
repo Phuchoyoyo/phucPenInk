90296d15f03ad06cbf54000dbb93bdd623c06573
jpxfrdd <91901550+jpxfrdd@users.noreply.github.com>
2023-09-19 08:23:27 -0400

    bruh

---
+++ phucPenInk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace phucPenInk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
