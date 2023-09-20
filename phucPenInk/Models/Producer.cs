2267ef2edf773ce0f872d612f5cd28fc1caeeb38
phucbaho314 <phucbaho314@gmail.com>
2023-09-20 07:38:56 -0400

    models changes

---
+++ phucPenInk/Models/Producer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+ namespace phucPenInk.Models
+
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string pictureUrl { get; set; }
+
+        public string productName { get; set; }
+
+        public string description { get; set; }
+    }
+}
